3ba16a8784ebd7e3c8e3ff76fd749a3656969433
Aditya Dani <aditya@portworx.com>
2017-03-20 21:40:59 +0000

    Modify the way tests are setup for secrets.
    - Instead of a common test suite for all the secrets implementation, define
     a common test interface for each implementation to implement.
    - The reason for such a change is different implementation handle Get and Put
      in a different way and we cannot have a common way of testing them.
    - The interface will enforce the implementation to test each of the basic functionalities.

--- test/secrets.go
+++ test/secrets.go
@@ -2,46 +2,14 @@ package test
 
 import (
 	"testing"
-
-	"github.com/libopenstorage/secrets"
 )
 
-func Run(secretBackendInit secrets.BackendInit, secretConfig map[string]interface{}, t *testing.T) {
-	secret, err := secretBackendInit(secretConfig)
-	if err != nil {
-		t.Fatalf("Unable to initialize secret backend: %v", err)
-	}
-	getPutKey(secret, t)
+type SecretTest interface {
+	TestPutSecret(t *testing.T) error
+	TestGetSecret(t *testing.T) error
 }
 
-func getPutKey(s secrets.Secrets, t *testing.T) {
-	data := make(map[string]interface{})
-	keyId := "hello"
-	data["key1"] = "value1"
-	data["key2"] = "value2"
-
-	if s == nil {
-		t.Fatalf("secrets is nil")
-	}
-	err := s.PutSecret(keyId, data, nil)
-	if err != nil {
-		t.Fatalf("Unable to put key into secrets: %v", err)
-	}
-
-	plainText, err := s.GetSecret(keyId, nil)
-	if err != nil {
-		t.Fatalf("Unable to get key from secrets: %v", err)
-	}
-	if len(data) != len(plainText) {
-		t.Errorf("Put and Get keys do not match")
-	}
-	for k, v := range plainText {
-		if o, exists := data[k]; !exists || o != v {
-			t.Errorf("Put and Get values do not match")
-		}
-	}
-	_, err = s.GetSecret("unknown_key", nil)
-	if err == nil {
-		t.Fatalf("Expected error when no secret key present")
-	}
+func Run(st SecretTest, t *testing.T) {
+	st.TestPutSecret(t)
+	st.TestGetSecret(t)
 }
--- vault/vault_test.go
+++ vault/vault_test.go
@@ -3,10 +3,66 @@ package vault
 import (
 	"testing"
 
+	"github.com/libopenstorage/secrets"
 	"github.com/libopenstorage/secrets/test"
 )
 
 func TestAll(t *testing.T) {
 	// Set the relevant environmnet fields for vault.
-	test.Run(New, nil, t)
+	vs, err := NewVaultSecretTest(nil)
+	if err != nil {
+		t.Fatalf("Unable to create a Vault Secret instance: %v", err)
+		return
+	}
+	test.Run(vs, t)
+}
+
+type vaultSecretTest struct {
+	s secrets.Secrets
+}
+
+func NewVaultSecretTest(secretConfig map[string]interface{}) (test.SecretTest, error) {
+	s, err := New(secretConfig)
+	if err != nil {
+		return nil, err
+	}
+	return &vaultSecretTest{s}, nil
+}
+
+func (v *vaultSecretTest) TestPutSecret(t *testing.T) error {
+	data := make(map[string]interface{})
+	keyId := "hello"
+	data["key1"] = "value1"
+	data["key2"] = "value2"
+
+	if v.s == nil {
+		t.Fatalf("secrets is nil")
+	}
+	err := v.s.PutSecret(keyId, data, nil)
+	if err != nil {
+		t.Fatalf("Unable to put key into secrets: %v", err)
+	}
+
+	plainText, err := v.s.GetSecret(keyId, nil)
+	if err != nil {
+		t.Fatalf("Unable to get key from secrets: %v", err)
+	}
+	if len(data) != len(plainText) {
+		t.Errorf("Put and Get keys do not match")
+	}
+	for k, v := range plainText {
+		if o, exists := data[k]; !exists || o != v {
+			t.Errorf("Put and Get values do not match")
+		}
+	}
+	_, err = v.s.GetSecret("unknown_key", nil)
+	if err == nil {
+		t.Fatalf("Expected error when no secret key present")
+	}
+	return nil
+}
+
+func (v *vaultSecretTest) TestGetSecret(t *testing.T) error {
+	// TestPutSecret does get testing as well
+	return nil
 }
