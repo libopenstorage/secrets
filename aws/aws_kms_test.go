cdccce239be1d80f940d3f95b4f5e6be6bdf61dc
Aditya Dani <aditya@portworx.com>
2017-03-21 20:48:50 +0000

    Add a few more unit tests for AWS KMS.

--- aws/aws_kms_test.go
+++ aws/aws_kms_test.go
@@ -1,9 +1,9 @@
 package aws
 
 import (
+	"io/ioutil"
 	"os"
 	"testing"
-	"io/ioutil"
 
 	"github.com/libopenstorage/secrets"
 	"github.com/libopenstorage/secrets/test"
@@ -17,8 +17,8 @@ func TestAll(t *testing.T) {
 	// Set the relevant environmnet fields for aws.
 	secretConfig := make(map[string]interface{})
 	// Fill in the appropriate keys and values
-	secretConfig[awsCMKey] = "xxxx"
-	secretConfig[awsRegionKey] = "us-<region>-1"
+	secretConfig[awsCMKey] = "<cmk-key>"
+	secretConfig[awsRegionKey] = "<aws-region>"
 	as, err := NewAwsSecretTest(secretConfig)
 	if err != nil {
 		t.Fatalf("Unable to create a AWS Secret instance: %v", err)
@@ -42,33 +42,51 @@ func NewAwsSecretTest(secretConfig map[string]interface{}) (test.SecretTest, err
 func (a *awsSecretTest) TestPutSecret(t *testing.T) error {
 	secretData := make(map[string]interface{})
 	secretData["key1"] = "value1"
+	// PutSecret with non-nil secretData
 	err := a.s.PutSecret(secretId, secretData, nil)
 	if err == nil {
 		t.Errorf("Expected PutSecret to fail. SecretData should always be null")
 	}
 
+	// A successful PutSecret
 	err = a.s.PutSecret(secretId, nil, nil)
 	if err != nil {
 		t.Errorf("Expected PutSecret to succeed. Failed with error: %v", err)
 	}
+	// Check if file with encrypted blob is created
 	_, err = os.Stat(secrets.SecretPath + secretId)
 	if os.IsNotExist(err) {
 		t.Errorf("Expected PutSecret to write a file with cipher text blob")
 	}
+
+	// PutSecret with already existing secretId
+	err = a.s.PutSecret(secretId, nil, nil)
+	if err != secrets.ErrSecretExists {
+		t.Errorf("Expected PutSecret to fail with ErrSecretExists error")
+	}
+
+	// PutSecret with input as a file path which already exists
+	err = a.s.PutSecret(secrets.SecretPath+secretId, nil, nil)
+	if err != secrets.ErrSecretExists {
+		t.Errorf("Expected PutSecret to fail with ErrSecretExists error")
+	}
+
 	return nil
 }
 
 func (a *awsSecretTest) TestGetSecret(t *testing.T) error {
+	// GetSecret with non-existant id
 	_, err := a.s.GetSecret("dummy", nil)
 	if err == nil {
 		t.Errorf("Expected GetSecret to fail. Invalid secretId")
 	}
 
-	plainText, err := a.s.GetSecret(secretId, nil)
+	// GetSecret using a secretId
+	plainText1, err := a.s.GetSecret(secretId, nil)
 	if err != nil {
 		t.Errorf("Expected GetSecret to succeed. Failed with error: %v", err)
 	}
-	if plainText == nil || len(plainText) != 1 {
+	if plainText1 == nil || len(plainText1) != 1 {
 		t.Errorf("Invalid PlainText was returned")
 	}
 
@@ -81,6 +99,7 @@ func (a *awsSecretTest) TestGetSecret(t *testing.T) error {
 			t.Errorf("Unable to read file")
 		}
 	}
+	// GetSecret with id as the actual cipherblob
 	plainText2, err := a.s.GetSecret(string(cipherBlob), nil)
 	if err != nil {
 		t.Errorf("Expected GetSecret to succeed. Failed with error: %v", err)
@@ -89,8 +108,36 @@ func (a *awsSecretTest) TestGetSecret(t *testing.T) error {
 		t.Errorf("Invalid PlainText was returned")
 	}
 
-	pT1, _ := plainText[secretId]
-	pT2, _ := plainText2[string(cipherBlob)]
+	checkPlainTextEquality(plainText1, plainText2, t)
+
+	// GetSecret with id as a file path where the cipherblob is present
+	plainText3, err := a.s.GetSecret(path, nil)
+	if err != nil {
+		t.Errorf("Expected GetSecret to succeed. Failed with error: %v", err)
+	}
+	if plainText3 == nil || len(plainText3) != 1 {
+		t.Errorf("Invalid PlainText was returned")
+	}
+
+	checkPlainTextEquality(plainText1, plainText3, t)
+	return nil
+}
+
+func checkPlainTextEquality(plainText1, plainText2 map[string]interface{}, t *testing.T) {
+	if len(plainText1) != len(plainText2) {
+		t.Errorf("Plaintext lengths do no match")
+	}
+	var (
+		pT1, pT2 interface{}
+	)
+	for _, v := range plainText1 {
+		pT1 = v
+		break
+	}
+	for _, v := range plainText2 {
+		pT2 = v
+		break
+	}
 	if pT1 == nil || pT2 == nil {
 		t.Errorf("Plaintext is null")
 	}
@@ -98,5 +145,4 @@ func (a *awsSecretTest) TestGetSecret(t *testing.T) error {
 	if len(pT1.([]byte)) != len(pT2.([]byte)) {
 		t.Errorf("Plain text do not match")
 	}
-	return nil
 }
