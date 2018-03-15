7f15a9bbe6b2896d2f2fb51f308ead1b0f68c5d2
Aditya Dani <aditya@portworx.com>
2018-03-14 18:32:03 -0700

    Revamp the AWS KMS implementation of the Secrets interface.
    AWS KMS cannot act as a secret store where we can store user data.
    It provides a mechanism to generate DataKeys that could be used
    for encrypting user data. In KMS when you generate a DataKey it returns
    1. [Private Key]: PlainText which should be used for encrypting data.
    2. [Public Key]: CipherText which should be used for subsequent calls to KMS to fetch the private key.
    
    - Add two persistence stores for AWS KMS to store the public key
      - filePersistenceStore - uses files on host
      - kvdbPersistenceStore - uses a key value database using portworx/kvdb
    
    - Add support for storing user data in a secret store
      - AWS KMS in conjunction with kvdb can store user data.
      - For every secretData to be stored, a new DataKey is generated in KMS.
      - PutSecret -> Using the private key and AES+GCM algorithm, the user data is encrypted and then stored in kvdb.
      - GetSecret -> Using the public key, the private key is fetched from KMS. The encrypted data stored in kvdb is then fetched
                     and using the same AES with GCM algorithm is used to decrypt the user data.

--- aws/aws_kms_test.go
+++ aws/aws_kms_test.go
@@ -1,25 +1,43 @@
 package aws
 
 import (
-	"io/ioutil"
 	"os"
 	"testing"
 
 	"github.com/libopenstorage/secrets"
 	"github.com/libopenstorage/secrets/test"
+	"github.com/portworx/kvdb"
+	"github.com/portworx/kvdb/mem"
 )
 
 const (
-	secretId = "openstorage_secret"
+	secretIdWithData    = "openstorage_secret_data"
+	secretIdWithoutData = "openstorage_secret"
 )
 
 func TestAll(t *testing.T) {
 	// Set the relevant environmnet fields for aws.
 	secretConfig := make(map[string]interface{})
 	// Fill in the appropriate keys and values
-	secretConfig[AwsCMKey] = "<cmk>"
-	secretConfig[AwsRegionKey] = "<region>"
+	secretConfig[AwsCMKKey] = os.Getenv(AwsCMKKey)
+	secretConfig[AwsRegionKey] = os.Getenv(AwsRegionKey)
+
+	os.RemoveAll(secrets.SecretPath + secretIdWithoutData)
+	// With filePersistenceStore
 	as, err := NewAwsSecretTest(secretConfig)
+	if err != nil {
+		t.Fatalf("Unable to create a AWS Secret instance: %v", err)
+		return
+	}
+	test.Run(as, t)
+
+	// With kvdbPersistenceStore
+	kv, err := kvdb.New(mem.Name, "openstorage/", nil, nil, nil)
+	if err != nil {
+		t.Fatalf("Unable to create a AWS Secret instance: %v", err)
+	}
+	secretConfig[KMSKvdbKey] = kv
+	as, err = NewAwsSecretTest(secretConfig)
 	if err != nil {
 		t.Fatalf("Unable to create a AWS Secret instance: %v", err)
 		return
@@ -42,31 +60,26 @@ func NewAwsSecretTest(secretConfig map[string]interface{}) (test.SecretTest, err
 func (a *awsSecretTest) TestPutSecret(t *testing.T) error {
 	secretData := make(map[string]interface{})
 	secretData["key1"] = "value1"
+	secretData["key2"] = "value2"
 	// PutSecret with non-nil secretData
-	err := a.s.PutSecret(secretId, secretData, nil)
-	if err == nil {
-		t.Errorf("Expected PutSecret to fail. SecretData should always be null")
+	err := a.s.PutSecret(secretIdWithData, secretData, nil)
+	if err != nil && err != ErrInvalidRequest {
+		t.Errorf("Expected PutSecret to not fail.: %v", err)
 	}
 
-	// A successful PutSecret
-	err = a.s.PutSecret(secretId, nil, nil)
+	// PutSecret with nil secretData
+	err = a.s.PutSecret(secretIdWithoutData, nil, nil)
 	if err != nil {
 		t.Errorf("Expected PutSecret to succeed. Failed with error: %v", err)
 	}
-	// Check if file with encrypted blob is created
-	_, err = os.Stat(secrets.SecretPath + secretId)
-	if os.IsNotExist(err) {
-		t.Errorf("Expected PutSecret to write a file with cipher text blob")
-	}
 
 	// PutSecret with already existing secretId
-	err = a.s.PutSecret(secretId, nil, nil)
-	if err != secrets.ErrSecretExists {
+	err = a.s.PutSecret(secretIdWithData, secretData, nil)
+	if err != secrets.ErrSecretExists && err != ErrInvalidRequest {
 		t.Errorf("Expected PutSecret to fail with ErrSecretExists error")
 	}
 
-	// PutSecret with input as a file path which already exists
-	err = a.s.PutSecret(secrets.SecretPath+secretId, nil, nil)
+	err = a.s.PutSecret(secretIdWithoutData, nil, nil)
 	if err != secrets.ErrSecretExists {
 		t.Errorf("Expected PutSecret to fail with ErrSecretExists error")
 	}
@@ -81,68 +94,32 @@ func (a *awsSecretTest) TestGetSecret(t *testing.T) error {
 		t.Errorf("Expected GetSecret to fail. Invalid secretId")
 	}
 
-	// GetSecret using a secretId
-	plainText1, err := a.s.GetSecret(secretId, nil)
-	if err != nil {
+	// GetSecret using a secretId with data
+	plainText1, err := a.s.GetSecret(secretIdWithData, nil)
+	if err != nil && err != secrets.ErrInvalidSecretId {
 		t.Errorf("Expected GetSecret to succeed. Failed with error: %v", err)
-	}
-	if plainText1 == nil || len(plainText1) != 1 {
-		t.Errorf("Invalid PlainText was returned")
-	}
-
-	path := secrets.SecretPath + secretId
-	cipherBlob := []byte{}
-	_, err = os.Stat(path)
-	if err == nil || os.IsExist(err) {
-		cipherBlob, err = ioutil.ReadFile(path)
-		if err != nil {
-			t.Errorf("Unable to read file")
+	} else if err == nil {
+		// We have got secretData
+		if plainText1 == nil {
+			t.Errorf("Invalid PlainText was returned")
+		}
+		v, ok := plainText1["key1"]
+		if !ok {
+			t.Errorf("Unexpected secretData")
+		}
+		str, ok := v.(string)
+		if !ok {
+			t.Errorf("Unexpected secretData")
+		}
+		if str != "value1" {
+			t.Errorf("Unexpected secretData")
 		}
 	}
-	// GetSecret with id as the actual cipherblob
-	plainText2, err := a.s.GetSecret(string(cipherBlob), nil)
+
+	// GetSecret using a secretId without data
+	_, err = a.s.GetSecret(secretIdWithoutData, nil)
 	if err != nil {
 		t.Errorf("Expected GetSecret to succeed. Failed with error: %v", err)
 	}
-	if plainText2 == nil || len(plainText2) != 1 {
-		t.Errorf("Invalid PlainText was returned")
-	}
-
-	checkPlainTextEquality(plainText1, plainText2, t)
-
-	// GetSecret with id as a file path where the cipherblob is present
-	plainText3, err := a.s.GetSecret(path, nil)
-	if err != nil {
-		t.Errorf("Expected GetSecret to succeed. Failed with error: %v", err)
-	}
-	if plainText3 == nil || len(plainText3) != 1 {
-		t.Errorf("Invalid PlainText was returned")
-	}
-
-	checkPlainTextEquality(plainText1, plainText3, t)
 	return nil
-}
-
-func checkPlainTextEquality(plainText1, plainText2 map[string]interface{}, t *testing.T) {
-	if len(plainText1) != len(plainText2) {
-		t.Errorf("Plaintext lengths do no match")
-	}
-	var (
-		pT1, pT2 interface{}
-	)
-	for _, v := range plainText1 {
-		pT1 = v
-		break
-	}
-	for _, v := range plainText2 {
-		pT2 = v
-		break
-	}
-	if pT1 == nil || pT2 == nil {
-		t.Errorf("Plaintext is null")
-	}
-
-	if len(pT1.([]byte)) != len(pT2.([]byte)) {
-		t.Errorf("Plain text do not match")
-	}
 }
