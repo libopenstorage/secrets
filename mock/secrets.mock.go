9eb67a0c64a83fe93f637f6b1be1d0114a613bc6
Humble Chirammal <hchiramm@redhat.com>
2021-11-19 12:02:20 +0530

    ibm: adjust mock tests using new testify update
    As part of the key protect client update new testify package version
    also has been picked up and the mock tests have been refreshed with
    the new version
    
    Signed-off-by: Humble Chirammal <hchiramm@redhat.com>

--- mock/secrets.mock.go
+++ mock/secrets.mock.go
@@ -5,9 +5,8 @@
 package mock
 
 import (
-	reflect "reflect"
-
 	gomock "github.com/golang/mock/gomock"
+	reflect "reflect"
 )
 
 // MockSecrets is a mock of Secrets interface
@@ -35,6 +34,7 @@ func (m *MockSecrets) EXPECT() *MockSecretsMockRecorder {
 
 // Decrypt mocks base method
 func (m *MockSecrets) Decrypt(arg0, arg1 string, arg2 map[string]string) (string, error) {
+	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "Decrypt", arg0, arg1, arg2)
 	ret0, _ := ret[0].(string)
 	ret1, _ := ret[1].(error)
@@ -43,11 +43,13 @@ func (m *MockSecrets) Decrypt(arg0, arg1 string, arg2 map[string]string) (string
 
 // Decrypt indicates an expected call of Decrypt
 func (mr *MockSecretsMockRecorder) Decrypt(arg0, arg1, arg2 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Decrypt", reflect.TypeOf((*MockSecrets)(nil).Decrypt), arg0, arg1, arg2)
 }
 
 // DeleteSecret mocks base method
 func (m *MockSecrets) DeleteSecret(arg0 string, arg1 map[string]string) error {
+	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "DeleteSecret", arg0, arg1)
 	ret0, _ := ret[0].(error)
 	return ret0
@@ -55,11 +57,13 @@ func (m *MockSecrets) DeleteSecret(arg0 string, arg1 map[string]string) error {
 
 // DeleteSecret indicates an expected call of DeleteSecret
 func (mr *MockSecretsMockRecorder) DeleteSecret(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "DeleteSecret", reflect.TypeOf((*MockSecrets)(nil).DeleteSecret), arg0, arg1)
 }
 
 // Encrypt mocks base method
 func (m *MockSecrets) Encrypt(arg0, arg1 string, arg2 map[string]string) (string, error) {
+	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "Encrypt", arg0, arg1, arg2)
 	ret0, _ := ret[0].(string)
 	ret1, _ := ret[1].(error)
@@ -68,11 +72,13 @@ func (m *MockSecrets) Encrypt(arg0, arg1 string, arg2 map[string]string) (string
 
 // Encrypt indicates an expected call of Encrypt
 func (mr *MockSecretsMockRecorder) Encrypt(arg0, arg1, arg2 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Encrypt", reflect.TypeOf((*MockSecrets)(nil).Encrypt), arg0, arg1, arg2)
 }
 
 // GetSecret mocks base method
 func (m *MockSecrets) GetSecret(arg0 string, arg1 map[string]string) (map[string]interface{}, error) {
+	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "GetSecret", arg0, arg1)
 	ret0, _ := ret[0].(map[string]interface{})
 	ret1, _ := ret[1].(error)
@@ -81,11 +87,13 @@ func (m *MockSecrets) GetSecret(arg0 string, arg1 map[string]string) (map[string
 
 // GetSecret indicates an expected call of GetSecret
 func (mr *MockSecretsMockRecorder) GetSecret(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetSecret", reflect.TypeOf((*MockSecrets)(nil).GetSecret), arg0, arg1)
 }
 
 // ListSecrets mocks base method
 func (m *MockSecrets) ListSecrets() ([]string, error) {
+	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "ListSecrets")
 	ret0, _ := ret[0].([]string)
 	ret1, _ := ret[1].(error)
@@ -94,11 +102,13 @@ func (m *MockSecrets) ListSecrets() ([]string, error) {
 
 // ListSecrets indicates an expected call of ListSecrets
 func (mr *MockSecretsMockRecorder) ListSecrets() *gomock.Call {
+	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ListSecrets", reflect.TypeOf((*MockSecrets)(nil).ListSecrets))
 }
 
 // PutSecret mocks base method
 func (m *MockSecrets) PutSecret(arg0 string, arg1 map[string]interface{}, arg2 map[string]string) error {
+	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "PutSecret", arg0, arg1, arg2)
 	ret0, _ := ret[0].(error)
 	return ret0
@@ -106,11 +116,13 @@ func (m *MockSecrets) PutSecret(arg0 string, arg1 map[string]interface{}, arg2 m
 
 // PutSecret indicates an expected call of PutSecret
 func (mr *MockSecretsMockRecorder) PutSecret(arg0, arg1, arg2 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "PutSecret", reflect.TypeOf((*MockSecrets)(nil).PutSecret), arg0, arg1, arg2)
 }
 
 // Rencrypt mocks base method
 func (m *MockSecrets) Rencrypt(arg0, arg1 string, arg2, arg3 map[string]string, arg4 string) (string, error) {
+	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "Rencrypt", arg0, arg1, arg2, arg3, arg4)
 	ret0, _ := ret[0].(string)
 	ret1, _ := ret[1].(error)
@@ -119,11 +131,13 @@ func (m *MockSecrets) Rencrypt(arg0, arg1 string, arg2, arg3 map[string]string, 
 
 // Rencrypt indicates an expected call of Rencrypt
 func (mr *MockSecretsMockRecorder) Rencrypt(arg0, arg1, arg2, arg3, arg4 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Rencrypt", reflect.TypeOf((*MockSecrets)(nil).Rencrypt), arg0, arg1, arg2, arg3, arg4)
 }
 
 // String mocks base method
 func (m *MockSecrets) String() string {
+	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "String")
 	ret0, _ := ret[0].(string)
 	return ret0
@@ -131,5 +145,6 @@ func (m *MockSecrets) String() string {
 
 // String indicates an expected call of String
 func (mr *MockSecretsMockRecorder) String() *gomock.Call {
+	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "String", reflect.TypeOf((*MockSecrets)(nil).String))
 }
