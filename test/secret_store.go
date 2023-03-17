b171d20296e8895a2512b467f2c90c64eaa5d384
Ludvik Prouza <lprouza@purestorage.com>
2023-03-17 17:51:20 +0100

    Add an integration test for the secrete key with prefix.

--- test/secret_store.go
+++ test/secret_store.go
@@ -12,6 +12,7 @@ import (
 
 type storeTest struct {
 	s                   secrets.SecretStore
+	secretPrefix        string
 	secretIdWithData    string
 	secretIdWithoutData string
 }
@@ -19,6 +20,7 @@ type storeTest struct {
 func RunForStore(store secrets.SecretStore, t *testing.T) {
 	st := &storeTest{
 		s:                   store,
+		secretPrefix:        "openstorage_secret_prefix_" + uuid.New(),
 		secretIdWithData:    "openstorage_secret_data_" + uuid.New(),
 		secretIdWithoutData: "openstorage_secret_" + uuid.New(),
 	}
@@ -32,31 +34,49 @@ func (a *storeTest) TestPutSecret(t *testing.T) {
 	secretData := make(map[string]interface{})
 	secretData["key1"] = "value1"
 	secretData["key2"] = "value2"
-	// PutSecret with non-nil secretData
+	// Set with non-nil secretData
 	err := a.s.Set(context.Background(), secrets.SecretKey{Name: a.secretIdWithData}, secretData)
-	assert.NoError(t, err, "Unexpected error on PutSecret")
+	assert.NoError(t, err, "Error on Set with data")
 
-	// PutSecret with nil secretData
+	// Set with nil secretData
 	err = a.s.Set(context.Background(), secrets.SecretKey{Name: a.secretIdWithoutData}, nil)
-	assert.NoError(t, err, "Expected PutSecret to succeed. Failed with error: %v", err)
+	assert.NoError(t, err, "Error on Set with nil data")
+
+	// Set with prefix
+	err = a.s.Set(context.Background(), secrets.SecretKey{Prefix: a.secretPrefix, Name: a.secretIdWithData}, secretData)
+	assert.NoError(t, err, "Error on Set with prefix")
 }
 
 func (a *storeTest) TestGetSecret(t *testing.T) {
-	// GetSecret with non-existant id
+	// Get with non-existent name
 	_, err := a.s.Get(context.Background(), secrets.SecretKey{Name: "dummy"})
-	assert.Error(t, err, "Expected GetSecret to fail")
+	assert.Error(t, err, "Expected Get with name")
 
-	// GetSecret using a secretId with data
+	// Get with non-existent name and prefix
+	_, err = a.s.Get(context.Background(), secrets.SecretKey{Prefix: "dummy", Name: "dummy"})
+	assert.Error(t, err, "Expected Get with name and prefix")
+
+	// Get using a name with data
 	plainText1, err := a.s.Get(context.Background(), secrets.SecretKey{Name: a.secretIdWithData})
-	assert.NoError(t, err, "Expected GetSecret to succeed")
-	assert.NotNil(t, plainText1, "Expected plainText to not be nil")
+	assert.NoError(t, err, "Get with name to succeed")
+	assert.NotNil(t, plainText1, "Get with name expected plainText to not be nil")
 	v, ok := plainText1["key1"]
-	assert.True(t, ok, "Unexpected secretData")
+	assert.True(t, ok, "Get with name unexpected secretData")
 	str, ok := v.(string)
-	assert.True(t, ok, "Unexpected secretData")
-	assert.Equal(t, str, "value1", "Unexpected secretData")
+	assert.True(t, ok, "Get with name unexpected secretData")
+	assert.Equal(t, str, "value1", "Get with name unexpected secretData")
 
-	// GetSecret using a secretId without data
+	// Get using a name and prefix with data
+	plainText1, err = a.s.Get(context.Background(), secrets.SecretKey{Prefix: a.secretPrefix, Name: a.secretIdWithData})
+	assert.NoError(t, err, "Get with name and prefix to succeed")
+	assert.NotNil(t, plainText1, "Get with name and prefix plainText to not be nil")
+	v, ok = plainText1["key1"]
+	assert.True(t, ok, "Get with name and prefix unexpected secretData")
+	str, ok = v.(string)
+	assert.True(t, ok, "Get with name and prefix unexpected secretData")
+	assert.Equal(t, str, "value1", "Get with name and prefix unexpected secretData")
+
+	// Get using a name without data
 	_, err = a.s.Get(context.Background(), secrets.SecretKey{Name: a.secretIdWithoutData})
 	assert.NoError(t, err, "Expected GetSecret to succeed")
 }
@@ -74,9 +94,13 @@ func (a *storeTest) TestDeleteSecret(t *testing.T) {
 	err = a.s.Delete(context.Background(), secrets.SecretKey{Name: a.secretIdWithoutData})
 	assert.NoError(t, err, "Expected DeleteSecret to succeed")
 
-	// GetSecret using a secretId without data
+	// Get using a secretId without data
 	_, err = a.s.Get(context.Background(), secrets.SecretKey{Name: a.secretIdWithoutData})
 	assert.EqualError(t, secrets.ErrInvalidSecretId, err.Error(), "Unexpected error on GetSecret after delete")
+
+	// Delete of existing name with prefix should succeed
+	err = a.s.Delete(context.Background(), secrets.SecretKey{Prefix: a.secretPrefix, Name: a.secretIdWithoutData})
+	assert.NoError(t, err, "Expected DeleteSecret to succeed")
 
 	// Delete of a non-existent key should also succeed
 	err = a.s.Delete(context.Background(), secrets.SecretKey{Name: "dummy"})
